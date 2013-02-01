c70cfcd563483581c5b76875cb2903ff1f13eb28
David Nolen <david.nolen@gmail.com>
2013-02-01 18:33:23 -0500

    get the JSTs in there

--- todo.js
+++ todo.js
@@ -1,9 +1,41 @@
 ;(function(global, j, _) {
+  var JST = {},
+  items = [];
+  
+
+  function addItem(x) {
+    x.id = items.length;
+    items.push(x);
+  }
+
+  function updateItem(x) {
+    items[x.id] = x;
+  }
+
+  function removeItem() {
+    items.splice(x.id,1);
+  }
+
+  function renderItems() {
+    JST["todos"](items)
+  }
+
+  function initJST() {
+    _(j(".jst")).each(function(jst) {
+      jst = j(jst);
+      JST[jst.attr("name")] = _.template(jst.text());
+    });
+  }
 
   function init() {
+    initJST();
     console.log("init");
   }
 
   j(document).ready(init);
+
+  // for debugging
+  global.JST = JST;
+  global.items = items;
 
 })(this, jQuery, Underscore);
