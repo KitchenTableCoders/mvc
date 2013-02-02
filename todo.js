1241a6c48ff6b022fcaf7a4e7200c2fa3d853868
David Nolen <david.nolen@gmail.com>
2013-02-02 11:17:40 -0500

    Initial commit

--- todo.js
+++ todo.js
@@ -1,3 +1,8 @@
+if(typeof window == "undefined") {
+  var Underscore = require("underscore"),
+      jQuery     = require("jquery");
+}
+
 ;(function(global, j, _) {
   var JST = {},
       items = [{checked: true, title: "Hello world!"},
@@ -32,11 +37,21 @@
     console.log("init");
   }
 
-  j(document).ready(init);
+  if(typeof window != "undefined") {
+    j(document).ready(init);
+  }
 
   // for debugging
-  global.JST = JST;
-  global.items = items;
-  global.renderItems = renderItems;
+  var toExport = {
+      JST: JST,
+      items: items,
+      renderItems: renderItems
+  };
 
+  if(typeof window != "undefined") {
+    global.Todo = {};
+    _.extend(global.Todo, toExport);
+  } else {
+    exports.Todo = toExport;
+  }
 })(this, jQuery, Underscore);
