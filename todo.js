2db4680253376d8600968ec85e50c93e897270e8
David Nolen <david.nolen@gmail.com>
2013-02-02 14:14:12 -0500

    latest

--- todo.js
+++ todo.js
@@ -3,28 +3,86 @@ if(typeof window == "undefined") {
 }
 
 ;(function(global, j, _) {
-  var JST = {},
-      items = [{checked: true, title: "Hello world!"},
-               {checked: false, title: "See you later!"}];
 
-  function addItem(x) {
-    items.push(x);
+  var _id = 0;
+  function guid() {
+    var guid = _id;
+    _id++;
+    return guid;
   }
 
-  function updateItem(x) {
-    items[x.id] = x;
+  var JST = {},
+      items = [{checked: false, title: "Get coffee", id: guid()},
+               {checked: false, title: "Get bagels", id: guid()},
+               {checked: false, title: "Get tea", id: guid()},
+               {checked: false, title: "Get sugar", id: guid()}];
+
+  var _listeners = [];
+  function addEvent(name, f) {
+    if(_listeners[name] == null) _listeners[name] = [];
+    _listeners[name].push(f);
+  }
+
+  function fireEvent(name) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    _(_listeners[name]).each(function(f) {
+      f.apply(null, args);
+    });
+  }
+
+  function addItem(x) {
+    x.checked = x.checked || false;
+    x.id = guid();
+    items.push(x);
+    fireEvent("changed");
+  }
+
+  function updateItem(id, x) {
+    items[id] = x;
+    fireEvent("changed");
   }
 
   function removeItem(id) {
     items.splice(id,1);
+    fireEvent("changed");
   }
 
-  function renderItems() {    
-    j("#main").html(JST["todos"]({items:items}));
-    var checked = _(items).filter(function(item) { return item.checked; });
-    j("#checked").html(JST["todos"]({items:checked}));
-    var unchecked = _(items).filter(function(item) { return !item.checked; });
-    j("#unchecked").html(JST["todos"]({items:unchecked}));
+  function findItem(id) {
+    return _(items).find(function(item) { 
+      return item.id === parseInt(id);
+    });
+  }
+
+  function markDone(id) {
+    var item = findItem(id);
+    item.checked = true;
+    fireEvent("changed");
+    fireEvent("todoDone", id, item.title);
+  }
+
+  function markNotDone(id) {
+    var item = findItem(id);
+    item.checked = false;
+    fireEvent("changed");
+    fireEvent("todoNotDone", id, item.title);
+  }
+
+  function notDone() {
+    return _(items).filter(function(item) {
+      return !item.checked;
+    });
+  }
+
+  function done() {
+    return _(items).filter(function(item) {
+      return item.checked;
+    });
+  }
+
+  function renderItems() {
+    j("#todo").html(JST["todos"]({items:notDone()}));
+    j("#done").html(JST["todos"]({items:done()}));
+    fireEvent("rendered");
   }
 
   function initJST() {
@@ -34,8 +92,39 @@ if(typeof window == "undefined") {
     });
   }
 
+  function addEvents() {
+    j(document)
+      .on("click", "#todo input[type=checkbox]", function(e) {
+        var li = j(e.target).parent("li");
+        markDone(li.data("todo-id"));
+      })
+      .on("click", "#done input[type=checkbox]", function(e) {
+        var li = j(e.target).parent("li");
+        markNotDone(li.data("todo-id"));
+      })
+  }
+
   function init() {
     initJST();
+    addEvents();
+
+    // initial render
+    renderItems();
+
+    // if data changes, re-render
+    addEvent("changed", renderItems);
+    addEvent("rendered", function() {
+      console.log("rendered!");
+    });
+
+    addEvent("todoDone", function(id, title) {
+      console.log("todo done event:", id, title);
+    });
+
+    addEvent("todoNotDone", function(id, title) {
+      console.log("todo notDone event:", id, title);
+    });
+
     console.log("init");
   }
 
@@ -49,7 +138,9 @@ if(typeof window == "undefined") {
       items: items,
       addItem: addItem,
       removeItem: removeItem,
-      renderItems: renderItems
+      renderItems: renderItems,
+      markDone: markDone,
+      markNotDone: markNotDone
   };
 
   if(typeof window != "undefined") {
